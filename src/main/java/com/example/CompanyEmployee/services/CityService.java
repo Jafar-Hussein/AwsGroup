7a8b691451faf0461c3f1447f1fa36ba2152343f
Jafar Hussein <Jafarhussein6@gmail.com>
2024-02-27 12:43:36 +0100

    Fixade class

--- src/main/java/com/example/CompanyEmployee/services/CityService.java
+++ src/main/java/com/example/CompanyEmployee/services/CityService.java
@@ -3,9 +3,10 @@ package com.example.CompanyEmployee.services;
 import com.example.CompanyEmployee.models.City;
 import com.example.CompanyEmployee.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class CityService {
 
     @Autowired

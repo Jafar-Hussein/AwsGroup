5e2fc43e850a962cb9089cc224fb242ca0b623de
Fredrik <91frerin@gafe.molndal.se>
2024-02-27 14:29:17 +0100

    KeyGenerator och KeyProperties

--- src/main/java/com/example/CompanyEmployee/utils/KeyGenerator.java
+++ src/main/java/com/example/CompanyEmployee/utils/KeyGenerator.java
@@ -1,4 +1,21 @@
 package com.example.CompanyEmployee.utils;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
 public class KeyGenerator {
+
+    public static KeyPair generateRsaKey() {
+
+        KeyPair keyPair;
+
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.genKeyPair();
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+        return keyPair;
+    }
 }
--- src/main/java/com/example/CompanyEmployee/utils/KeyProperties.java
+++ src/main/java/com/example/CompanyEmployee/utils/KeyProperties.java
@@ -1,4 +1,35 @@
 package com.example.CompanyEmployee.utils;
 
+import org.springframework.stereotype.Component;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Component
 public class KeyProperties {
+
+    private RSAPublicKey publicKey;
+    private RSAPrivateKey privateKey;
+
+    public KeyProperties() {
+        KeyPair keyPair = KeyGenerator.generateRsaKey();
+        this.publicKey = (RSAPublicKey) keyPair.getPublic();
+        this.privateKey = (RSAPrivateKey) keyPair.getPrivate();
+    }
+    public RSAPublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(RSAPublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public RSAPrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(RSAPrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
 }

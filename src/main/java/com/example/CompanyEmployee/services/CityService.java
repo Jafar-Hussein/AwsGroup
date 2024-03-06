4223461ba45f1ad1a510359c40e3120ce6bd82a2
clarabrorson <88clabro@gafe.molndal.se>
2024-03-06 09:38:07 +0100

    Update CityService.java

--- src/main/java/com/example/CompanyEmployee/services/CityService.java
+++ src/main/java/com/example/CompanyEmployee/services/CityService.java
@@ -2,7 +2,6 @@ package com.example.CompanyEmployee.services;
 
 import com.example.CompanyEmployee.models.City;
 import com.example.CompanyEmployee.repository.CityRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
